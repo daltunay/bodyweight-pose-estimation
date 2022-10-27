ffdc3c65b009f0fa7e45758c9a1c5eaa62d824f0
Daniel Altunay <daniel.altunay@gmail.com>
2022-10-27 22:47:56 +0200

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+import sys
+
+from src.data import Video
+from src.resources.joints import JOINTS
+
+sys.path.append("src/models")
+
+
+def main() -> None:
+    model = pickle.load(open("models/model.pkl", "rb"))
+
+    vid = Video(path=1)
+    vid.get_landmarks(model_complexity=2, show=True)
+    vid.extract_coordinates()
+    vid.extract_angles(JOINTS)
+    vid._angles.smooth(inplace=True)
+    X = vid._angles.frame
+
+    pred = model._predict(X)
+    label = model.predict(X)
+
+    print(pred)
+    return label
+
+
+if __name__ == "__main__":
+    main()
