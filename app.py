fb4e6b26a3c54a3bd81ac14d8454a57d297f18d5
Daniel Altunay <daniel.altunay@gmail.com>
2022-10-28 00:17:27 +0200

    feat: added model complexity as param

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ from src.resources.joints import JOINTS
 
 sys.path.append("src/models")
 
+MODEL_COMPLEXITY = 1  # 0, 1 or 2
+
 
 def main() -> None:
     model = pickle.load(open("models/model.pkl", "rb"))
 
     vid = Video(path=1)
-    vid.get_landmarks(model_complexity=2, show=True)
+    vid.get_landmarks(model_complexity=MODEL_COMPLEXITY, show=True)
     vid.extract_coordinates()
     vid.extract_angles(JOINTS)
     vid._angles.smooth(inplace=True)
